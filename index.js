cb4cd002cea7a73869c4d7278d06491d3cb82055
Vanessa Bonilha <81935432+bonilhav@users.noreply.github.com>
2021-06-25 14:09:51 -0600

    questions const and packages

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief description of your project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What dependencies does the user need to install?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What usage information does the user need to know?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What information does the user need to know about contributing to this reporsitory?',
+        name: 'contributors',
+    },
+    {
+        type: 'input',
+        message: 'If you have written any tests please provide them here',
+        name: 'tests',
+    },
+    {
+        type: 'list',
+        message: 'What license should this project have?',
+        name: 'license',
+        choices: [
+            'MIT License',
+            'Apache License',
+            'GNU General Public License',
+            'Unlicensed',
+        ]
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
